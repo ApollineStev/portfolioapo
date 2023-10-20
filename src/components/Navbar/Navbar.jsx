67107648ea0dd8e5a3db1146269c1b4c260f15ad
ApollineStev <apollinecanard@gmail.com>
2023-10-20 11:05:54 +0200

    working?

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./Navbar.css";
+
+
+function Navbar() {
+  
+
+  return (
+    <nav className="d-flex justify-content-between align-items-center">
+      
+        <img src="./Asterlight.png" alt="logo.png" />
+        
+      
+      <div className="navChoices">
+        <a href="#presentation">Présentation</a>
+        <a href="#competences">Compétences</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </div>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Homepage/HomePage.jsx
@@ -0,0 +1,23 @@
+import "./HomePage.css";
+import React from "react";
+import {
+  MDBCarousel,
+  MDBCarouselItem,
+} from 'mdb-react-ui-kit';
+
+function HomePage() {
+  return (
+    <div>
+
+      <section id = "intro"
+         style={ { }}>
+        <h1>Apolline Stevenin</h1>
+        <h3>développeur web junior</h3>
+      </section>
+
+      
+    </div>
+  );
+}
+
+export default HomePage;
